6a5f346907d361bf1c557c88d0d670c952f3c365
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-22 14:21:36 +0100

    Add IConsoleBoard interface
    Provide console board with 2 properties for storing buffer cells
    and 3 methods controlling the board.

---
+++ RQ-0x0-TicTacToe/IConsoleBoard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RQ_0x0_TicTacToe
+{
+    public interface IConsoleBoard
+    {
+        public char[] Board { get; protected set; }
+
+        public int CurrentPosition { get; protected set; }
+
+        public void MoveToPosition(int index);
+
+        public void UpdatePosition();
+
+        public void WriteBoard();
+    }
+}
