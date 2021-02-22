3106d6814633360de6daddb89e730cdecb04d57c
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-22 15:22:44 +0100

    Update IConsoleBoard interface to match Console class handles

--- RQ-0x0-TicTacToe/IConsoleBoard.cs
+++ RQ-0x0-TicTacToe/IConsoleBoard.cs
@@ -6,11 +6,7 @@ namespace RQ_0x0_TicTacToe
 {
     public interface IConsoleBoard
     {
-        public char[] Board { get; protected set; }
-
-        public int CurrentPosition { get; protected set; }
-
-        public void MoveToPosition(int index);
+        public void MoveToPosition(int x, int y);
 
         public void UpdatePosition();
 
